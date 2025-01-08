545a8a4ee5009f3fa9fb10960d17166f553d164d
Alineboy <alineboy240@gmail.com>
2025-01-08 23:02:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "14810154"))
+  API_HASH = os.environ.get("API_HASH", "8df64055e6f5cc4551bfa20027799c92")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7737002477:AAFE3dG3F10zlIpM5Fdp_BOr2g6R6hNki1g")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "et_byme_file_store_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "Entertainment_time_ByMe"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "shrinkearn.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "b3e559c4e738de42b0a3e038ffa25731e3b33ad1")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1985104467"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://aniketmaurya23356:xzp4uf76Xx5Xaa5@cluster0.kdeg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1001717129525")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002154405024"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](http://t.me/et_byme_file_store_bot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
@@ -31,12 +31,12 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 │
 ╰──────[ 😎 ]───────────⍟
 """
-  ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+  ABOUT_ME_TEXT = f"""
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [ME](https://t.me/acuteboy_03)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/acuteboy_03)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
